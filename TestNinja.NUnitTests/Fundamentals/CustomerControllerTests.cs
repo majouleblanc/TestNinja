c20c29c91ff48c803565fabcdf5f4b5f1de18423
YoussefNabet <s112695@ap.be>
2020-12-17 23:38:45 +0100

    Testing return type of methods

---
+++ TestNinja.NUnitTests/Fundamentals/CustomerControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.NUnitTests.Fundamentals
+{
+    [TestFixture]
+    class CustomerControllerTests
+    {
+        private CustomerController _customController;
+        [SetUp]
+        public void SetUp()
+        {
+            _customController = new CustomerController();
+        }
+        [Test]
+        public void GetCustomer_IfIdIsZero_ReturnsNotFound()
+        {
+            var result = _customController.GetCustomer(0);
+
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IfIdIsNotZero_ReturnsOk()
+        {
+            var result = _customController.GetCustomer(1);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+
+            Assert.That(result, Is.InstanceOf<Ok>());
+        }
+    }
+}
---
+++ TestNinja.UnitTests/Fundamentals/CustomerControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.MSUnitTests.Fundamentals
+{
+    [TestClass]
+    class CustomerControllerTests
+    {
+        private CustomerController _customerController;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _customerController = new CustomerController();
+        }
+
+        [TestMethod]
+        public void GetCustomer_IfIdIsZero_ReturnsNotFound()
+        {
+            var result = _customerController.GetCustomer(0);
+
+            Assert.AreEqual(result.GetType(), typeof(NotFound));
+
+            Assert.IsInstanceOfType(result.GetType(), typeof(NotFound));
+        }
+
+        [TestMethod]
+        public void GetCustomer_IfIdIsNotZero_ReturnsOk()
+        {
+            var result = _customerController.GetCustomer(1);
+
+            Assert.AreEqual(result.GetType(), typeof(Ok));
+
+            Assert.IsInstanceOfType(result.GetType(), typeof(Ok));
+        }
+    }
+}
