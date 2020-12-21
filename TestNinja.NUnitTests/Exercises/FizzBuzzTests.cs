44c6349426b4f212b5e68a70cf354cf0d17fd197
YoussefNabet <s112695@ap.be>
2020-12-21 11:25:26 +0100

    Exercise: Test FizzBuzz Class

---
+++ TestNinja.NUnitTests/Exercises/FizzBuzzTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.NUnitTests.Exercises
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_NumberDivisileBy3And5_returnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_NumberDivisibleBy3Only_returnFizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_NumberDivisibleBy5Only_returnBuzz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(4)]
+        [TestCase(7)]
+        [TestCase(8)]
+        public void GetOutput_NumberNotDivisibleBy3Or5_returnNumber(int number)
+        {
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.That(result, Is.EqualTo(number.ToString()));
+        }
+    }
+}
---
+++ TestNinja.UnitTests/Fundamentals/FizzBuzzTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.MSUnitTests.Fundamentals
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void GetOutput_NumberDivisileBy3And5_returnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+
+            Assert.AreEqual(result, "FizzBuzz");
+        }
+
+        [TestMethod]
+        public void GetOutput_NumberDivisibleBy3Only_returnFizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+
+            Assert.AreEqual(result, "Fizz");
+        }
+
+        [TestMethod]
+        public void GetOutput_NumberDivisibleBy5Only_returnBuzz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+
+            Assert.AreEqual(result, "Buzz");
+
+        }
+
+        [TestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(4)]
+        [DataRow(7)]
+        [DataRow(8)]
+        public void GetOutput_NumberNotDivisibleBy3Or5_returnNumber(int number)
+        {
+            var result = FizzBuzz.GetOutput(number);
+
+            Assert.AreEqual(result, number.ToString());
+        }
+
+    }
+}
