9c95f6e0932063a2dcd2b4f7c28d48d9bb144294
YoussefNabet <s112695@ap.be>
2020-12-28 01:03:35 +0100

    Exercise Test DemeritPoints Class

---
+++ TestNinja.NUnitTests/Exercises/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.NUnitTests.Exercises
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTest
+    {
+        private DemeritPointsCalculator _demerit;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _demerit = new DemeritPointsCalculator();
+        }
+
+        [Test]
+        [TestCase(-4)]
+        [TestCase(300)]
+        public void CalculateDemeritPoints_SpeedNegatif_ThrowsArgumentOutOfRangeException(int speed)
+        {
+            if (speed <0 || speed >300)
+            {
+                Assert.Throws<ArgumentOutOfRangeException>(() => _demerit.CalculateDemeritPoints(-4));
+            }
+        }
+
+        
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(50, 0)]
+        [TestCase(65,0)]
+        [TestCase(70,1)]
+        [TestCase(75,2)]
+        [TestCase(80, 3)]
+        public void CalculateDemeritPoints_SpeedGraiterThan0_ReturnsDemeritPoints(int speed, int expected)
+        {
+            var result = _demerit.CalculateDemeritPoints(speed);
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ TestNinja.UnitTests/Exercices/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.MSUnitTests.Exercices
+{
+    [TestClass]
+    public class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator _demerite;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _demerite = new DemeritPointsCalculator();
+        }
+
+        [TestMethod]
+        [DataRow(-4)]
+        public void CalculateDemeritPoints_SpeedNegatif_ThrowsArgumentOutOfRangeException(int speed)
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _demerite.CalculateDemeritPoints(speed));
+        }
+
+        [TestMethod]
+        [DataRow(0,0)]
+        [DataRow(50,0)]
+        [DataRow(65,0)]
+        [DataRow(70, 1)]
+        [DataRow(75, 2)]
+        [DataRow(80, 3)]
+        public void CalculateDemeritPoints_SpeedGraiterThan0_ReturnsDemeritPoints(int speed, int expected)
+        {
+            var result = _demerite.CalculateDemeritPoints(speed);
+
+            Assert.AreEqual(result, expected);
+        }
+
+    }
+}
