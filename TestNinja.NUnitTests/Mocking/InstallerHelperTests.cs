6fcd12b38d4f78b72aaf8b05c2c3d282571871fe
YoussefNabet <s112695@ap.be>
2021-01-21 13:16:43 +0100

    InstallerHelper class tests

---
+++ TestNinja.NUnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using TestNinja.Mocking;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using System.Net;
+
+namespace TestNinja.Mocking.Tests
+{
+    [TestFixture()]
+    public class InstallerHelperTests
+    {
+        private Mock<IFileDownloader> _mockFileDownloader;
+        private InstallerHelper _installerHelper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockFileDownloader = new Mock<IFileDownloader>();
+            _installerHelper = new InstallerHelper(_mockFileDownloader.Object);
+        }
+
+        [Test()]
+        public void DownloadInstaller_DownloadFails_ReturnsFalse()
+        {
+            _mockFileDownloader.Setup(fd => fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>())).Throws<WebException>();
+
+            var result = _installerHelper.DownloadInstaller("", null);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test()]
+        public void DownloadInstaller_DownloadSucceeded_ReturnsTrue()
+        {
+            _mockFileDownloader.Setup(fd => fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>()));
+
+            Assert.IsTrue(_installerHelper.DownloadInstaller("", ""));
+        }
+    }
+}
